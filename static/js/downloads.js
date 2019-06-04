7ed664566e8f251ed1f7dea1bf5272a711381bb8
Kry9toN <dhimasbagusprayoga@gmail.com>
2019-06-04 14:37:57 +0700

    add footer and floating button

--- static/js/downloads.js
+++ static/js/downloads.js
@@ -5,10 +5,14 @@ const toogleMenu = () => {
 }
 
     // init collapsible
-    let elems = document.querySelector('.collapsible');
+    const elems = document.querySelector('.collapsible');
     M.Collapsible.init(elems);
   
     // init sidenav
-    let sidenav = document.querySelectorAll('.sidenav');
+    const sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+	
+	// fload buttom
+	const abuttom = document.querySelectorAll('.fixed-action-btn');
+    M.FloatingActionButton.init(abuttom);
 
